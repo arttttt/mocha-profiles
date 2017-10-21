115ed9f82c86343020cc942013b055a17c4fb305
arttttt <artem-bambalov@yandex.ru>
2017-10-21 23:12:48 +0300

    UndervoltageManager: add missed file after rename

---
+++ app/src/main/java/com/android/artt/mochaprofiles/undervoltage/UndervoltageManager.kt
@@ -0,0 +1,51 @@
+package com.android.artt.mochaprofiles.undervoltage
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.util.Log
+import com.android.artt.mochaprofiles.base.ManagerBase
+import com.android.artt.mochaprofiles.utils.SU
+
+class UndervoltageManager(context: Context) : ManagerBase(context) {
+    val TAG
+        get() = "MochaProfiles"
+
+    private val CPU_PATH
+        get() = "/sys/kernel/debug/clock/vdd_core_offs"
+    private val GPU_PATH
+        get() = "/sys/kernel/debug/clock/vdd_gpu_offs"
+
+    private val reduceCpuVoltageValue = -80
+    private val reduceGpuVoltageValue = -45
+
+    override val PREFERENCES
+        get() = "undervolt_preferences"
+    val UNDERVOLT_KEY
+        get() = "enabled"
+
+    @SuppressLint("CommitPrefEdits")
+    fun enableUndervolting(enabled: Boolean) {
+        if (SU.instance.getSuAccess() && SU.instance.rootAccess()) {
+            if (isUndervoltingEnabled() != enabled) {
+                with(mSharedPreferences.edit()) {
+                    putBoolean(UNDERVOLT_KEY, enabled)
+                    apply()
+                }
+            }
+
+            val cpuVoltageDeviation = if (enabled) reduceCpuVoltageValue else 0
+            val gpuVoltageDeviation = if (enabled) reduceGpuVoltageValue else 0
+
+            Log.d(TAG, "Undervoltage: enabled: $enabled\n cpu: $cpuVoltageDeviation mv\n gpu: $gpuVoltageDeviation mv")
+
+            with(SU.instance) {
+                writeFile(CPU_PATH, cpuVoltageDeviation)
+                writeFile(GPU_PATH, gpuVoltageDeviation)
+
+                close()
+            }
+        }
+    }
+
+    fun isUndervoltingEnabled(): Boolean = mSharedPreferences.getBoolean(UNDERVOLT_KEY, false)
+}
